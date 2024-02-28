acae682a9aac1a60572fbccdf30f2f170d593c88
chipopo <huyhq@coin98.finance>
2024-02-28 10:57:39 +0700

    fix: ui bottom

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -3,7 +3,9 @@ interface ContainerProps {
 }
 
 const Container: React.FC<ContainerProps> = ({ children }: any) => {
-  return <div className="mx-auto max-w-7xl px-8 mt-5">{children}</div>;
+  return (
+    <div className="mx-auto max-w-7xl px-8 mt-5 pb-20 md:pb-10">{children}</div>
+  );
 };
 
 export default Container;
